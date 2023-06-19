9bcf64c20b75525fe7d039d39ee627b955831b4a
vats98754 <vats98754@gapps.uwcsea.edu.sg>
2023-06-18 22:23:29 -0400

    feat: react app for simple token created

--- src/App.js
+++ src/App.js
@@ -1,23 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
+import Web3 from "web3";
+import MyContract from "./SimpleToken.sol";
 
 function App() {
+  const [contract, setContract] = useState(null);
+  const [number, setNumber] = useState(0);
+
+  useEffect(() => {
+    async function loadContract() {
+      try {
+        const web3 = new Web3(Web3.givenProvider || "http://localhost:8545");
+        const networkId = await web3.eth.net.getId();
+        const deployedNetwork = MyContract.networks[networkId];
+        const instance = new web3.eth.Contract(
+          MyContract.abi,
+          deployedNetwork && deployedNetwork.address
+        );
+        setContract(instance);
+      } catch (error) {
+        console.error("Error loading contract: ", error);
+      }
+    }
+
+    loadContract();
+  }, []);
+
+  const setNumberHandler = async () => {
+    try {
+      await contract.methods.setNumber(number).send({ from: "0xae2Fc483527B8EF99EB5D9B44875F005ba1FaE13" });
+    } catch (error) {
+      console.error("Error setting number: ", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <h1>Quantstamp App</h1>
+      <input type="number" value={number} onChange={(e) => setNumber(e.target.value)} />
+      <button onClick={setNumberHandler}>Set Number</button>
     </div>
   );
 }
