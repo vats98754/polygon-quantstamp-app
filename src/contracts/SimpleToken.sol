8616a5e33bf7119fba82ddbba695327812fb1500
vats98754 <vats98754@gapps.uwcsea.edu.sg>
2023-06-19 12:28:45 -0400

    feat: add Quantstamp auditing for secure exchange of SimpleTokens

--- src/contracts/SimpleToken.sol
+++ src/contracts/SimpleToken.sol
@@ -7,4 +7,24 @@ contract SimpleToken is ERC20 {
     constructor() ERC20("SimpleToken", "ST") {
         _mint(msg.sender, 1000000000000000000000000); // Mint 1,000,000 tokens
     }
+    
+    function auditContract() external {
+        // Get the address of the deployed contract to be audited
+        address contractToAudit = address(this);
+
+        // Create an instance of the QuantstampAuditData contract
+        QSPAuditData qspAuditData = new QSPAuditData();
+
+        // Register the contract to be audited
+        qspAuditData.registerContract(contractToAudit);
+
+        // Perform the security audit
+        qspAuditData.auditContract();
+
+        // Get the audit report data
+        QSPAuditReportData qspAuditReportData = QSPAuditReportData(qspAuditData.getAuditReport());
+
+        // Access and use the audit report data as needed
+        // ...
+    }
 }
